cfa6fa439e2ed50c8201d56b59fb840594e7d911
Kit <nikitabit@yandex.ru>
2023-01-31 11:41:51 +0700

    Modify after review

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -30,24 +30,24 @@ def show_all_pokemons(request):
 
     moscow_date_time = localtime(timezone=timezone("Europe/Moscow"))
 
-    db_pokemon_entity = PokemonEntity.objects.filter(appear_at__lt=moscow_date_time,\
+    pokemon_entities = PokemonEntity.objects.filter(appear_at__lt=moscow_date_time,\
                         disappeared_at__gt=moscow_date_time)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for entity in db_pokemon_entity:
+    for entity in pokemon_entities:
         add_pokemon(
             folium_map, entity.lat,
             entity.lon,
             request.build_absolute_uri(entity.pokemon.image.url)
         )
     pokemons_on_page = []
-    db_pokemons = Pokemon.objects.all()
-    for db_pokemon in db_pokemons:
-        image = db_pokemon.image.url if db_pokemon.image else ""
+    pokemons = Pokemon.objects.all()
+    for pokemon in pokemons:
+        image = pokemon.image.url if pokemon.image else DEFAULT_IMAGE_URL
         pokemons_on_page.append({
-            'pokemon_id': db_pokemon.id,
+            'pokemon_id': pokemon.id,
             'img_url': image,
-            'title_ru': db_pokemon.title,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
